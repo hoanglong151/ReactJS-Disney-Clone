da4ec0386f4f49b42fe3ed9c58a0175a1a897eeb
187PM13956 - Nguyen Pham Hoang Long - K24T-PM1 <long.187pm13956@vanlanguni.vn>
2022-01-15 17:24:18 +0700

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`
+
+const Logo = styled.img`
+  width: 80px;
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a{
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img{
+      height: 20px
+    }
+
+    span{
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after{
+        content: "";
+        position: absolute;
+        height: 2px;
+        background: #fff;
+        bottom: -6px;
+        left: 0;
+        right: 0;
+        transform: scaleX(0);
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+      }
+    }
+
+    &:hover span:after{
+      transform: scaleX(1);
+    }
+  }
+`
+
+const UserImg = styled.img`
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  cursor: pointer;
+`
